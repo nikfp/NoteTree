058567815aab55b9ce99d725e05d2076222bfff0
nikfp <nikprotsman@gmail.com>
2023-01-06 22:41:42 -0700

    Got basic auth working

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,20 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from "@prisma/client"
+
+import type { Adapter } from "@auth/core/adapters"
+
+const prisma = new PrismaClient();
 
 export const handle = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "database",
+    generateSessionToken: () => {
+      return crypto.randomUUID();
+    }
+  },
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
 })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<h1>SvelteKit Auth Example</h1>
+<script lang="ts">
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+</script>
+
 <p>
-  This is an example site to demonstrate how to use <a
-    href="https://kit.svelte.dev/">SvelteKit</a
-  >
-  with <a href="https://sveltekit.authjs.dev">SvelteKit Auth</a> for authentication.
+	{#if Object.keys($page.data.session || {}).length}
+		{#if $page.data.session.user.image}
+			<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
+		{/if}
+		<span class="signedInText">
+			<small>Signed in as</small><br />
+			<strong>{$page.data.session.user.email || $page.data.session.user.name}</strong>
+		</span>
+		<button on:click={() => signOut()} class="button">Sign out</button>
+	{:else}
+		<span class="notSignedInText">You are not signed in</span>
+		<button on:click={() => signIn('github')}>Sign In with GitHub</button>
+	{/if}
 </p>
