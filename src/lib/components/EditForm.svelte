8645b5c9097aae0426fcd46946064d6e52c0309b
nikfp <nikprotsman@gmail.com>
2023-01-11 13:58:05 -0700

    turned input form into a component

---
+++ src/lib/components/EditForm.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { enhance } from "$app/forms"
+</script>
+
+<form use:enhance method="post">
+  <p>
+    <label for="title">Title</label>
+    <input id="title" type="text" name="title" />
+  </p>
+  <p>
+    <label for="body">Body</label>
+    <textarea id="body" name="body" />
+  </p>
+  <button type="submit">submit</button>
+</form>
--- src/routes/note/new/+page.svelte
+++ src/routes/note/new/+page.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
   import type { ActionData } from "./$types"
-  import { enhance } from "$app/forms"
+  import EditForm from "$lib/components/EditForm.svelte"
   export let form: ActionData
 </script>
 
 <h1>You hit the new note page!</h1>
-<form use:enhance method="post">
-  <p>
-    <label for="title">Title</label>
-    <input id="title" type="text" name="title" />
-  </p>
-  <p>
-    <label for="body">Body</label>
-    <textarea id="body" name="body" />
-  </p>
-  <button type="submit">submit</button>
-</form>
+<EditForm />
 {#if form?.error}
   <ul>
     {#each form.errors as error}
