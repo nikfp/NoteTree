6c6efd1e177edd7565654c182ae2a7f0301f07e1
nikfp <nikprotsman@gmail.com>
2023-01-12 15:32:41 -0700

    formatting and changing env variable picker

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,26 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { prisma } from '$lib/database'
+import { env } from "$env/dynamic/private"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "$lib/database"
 
 export const handle = SvelteKitAuth({
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "database",
     generateSessionToken: () => {
-      return crypto.randomUUID();
-    }
+      return crypto.randomUUID()
+    },
   },
   callbacks: {
-    session: async ({session, user}) => {
-      if(session && user && session.user) {
-        session.user.id = user.id;
+    session: async ({ session, user }) => {
+      if (session && user && session.user) {
+        session.user.id = user.id
       }
-      return session;
-    }
+      return session
+    },
   },
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GitHub({ clientId: env.GITHUB_ID, clientSecret: env.GITHUB_SECRET }),
+  ],
 })
