53f3ba9bf59078f830e952daf528487e363546e0
nikfp <nikprotsman@gmail.com>
2023-01-16 12:56:38 -0700

    removed some old trpc stuff

--- src/lib/trpc-server/index.ts
+++
@@ -1,9 +0,0 @@
-import * as trpc from '@trpc/server'
-
-export const t = trpc.initTRPC.create();
-
-export const router = t.router({
-  sayHello: t.procedure.query(() => "Hello from TRPC"),
-})
-
-export type Router = typeof router;
