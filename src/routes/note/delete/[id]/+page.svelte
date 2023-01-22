4d26ecd2c66f4b7dbb45f99be896a64906ea83e1
nikfp <nikprotsman@gmail.com>
2023-01-22 13:08:40 -0700

    Updated alert CSS - removing bootstrap

--- src/routes/note/delete/[id]/+page.svelte
+++ src/routes/note/delete/[id]/+page.svelte
@@ -16,13 +16,13 @@
   <p>You must delete or move the leaf notes before deleting this note!</p>
   <a href={`/note/${id}`} class="btn btn-outline-primary">Go to Note page</a>
 {:else}
-  <Alert color="danger">
+  <div class="alert alert-danger">
     <h2>Delete this note? This cannot be undone!</h2>
     <form method="post">
       <input name="id" id="id" type="text" hidden value={id} />
       <button type="submit" class="btn btn-danger">Delete</button>
     </form>
-  </Alert>
+  </div>
 {/if}
 
 {#if form?.error}
@@ -32,3 +32,19 @@
     {/each}
   </ul>
 {/if}
+
+<style>
+  .alert {
+    position: relative;
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+  }
+
+  .alert-danger {
+    color: var(--clr-alert);
+    background-color: var(--clr-alert-background);
+    border-color: var(--clr-alert-border);
+  }
+</style>
